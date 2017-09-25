a0cdde0d3e0889eb5bfbc647982e0dc463cb2b18
s-vde <sanne-acc@posteo.net>
2017-09-25 13:35:59 +0200

    [format] src/vector_clock

--- src/vector_clock.cpp
+++ src/vector_clock.cpp
@@ -1,86 +1,80 @@
 
 #include "vector_clock.hpp"
 
-// UTILS
-#include "algo.hpp"
+#include <algo.hpp>
 
-// STL
 #include <assert.h>
 
-namespace exploration
-{
-   //-------------------------------------------------------------------------------------
-   
-   VectorClock::VectorClock(std::size_t n)
-   : datastructures::fixed_size_vector<int>(n, 0)
-   {
-   }
-   
-   //-------------------------------------------------------------------------------------
-   
-   void VectorClock::max(const VectorClock& other)
-   {
-      /// @pre mSize == other.size()
-      assert(size() == other.size());
-      for (index_t i = 0; i < size(); ++i)
-      {
-         (*this)[i] = std::max((*this)[i], other[i]);
-      }
-   }
-   
-   //-------------------------------------------------------------------------------------
-   
-   void VectorClock::filter_values_greater_than(const VectorClock& other)
-   {
-      /// @pre mSize == other.size()
-      assert(size() == other.size());
-      for (index_t i = 0; i < size(); ++i)
-      {
-         if ((*this)[i] <= other[i])
-         {
-            (*this)[i] = 0;
-         }
-      }
-   }
-   
-   //-------------------------------------------------------------------------------------
-    
-   VectorClock::value_t min_element(const VectorClock& clock)
-   {
-      return *std::min_element(clock.begin(), clock.end());
-   }
-   
-   //-------------------------------------------------------------------------------------
-    
-   VectorClock::value_t max_element(const VectorClock& clock)
-   {
-      return *std::max_element(clock.begin(), clock.end());
-   }
-   
-   //-------------------------------------------------------------------------------------
 
-   VectorClock::indices_t indices_such_that(const VectorClock& clock,
-                                            const value_predicate_t& pred)
+namespace exploration {
+
+//--------------------------------------------------------------------------------------------------
+
+VectorClock::VectorClock(std::size_t n)
+: datastructures::fixed_size_vector<int>(n, 0)
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void VectorClock::max(const VectorClock& other)
+{
+   /// @pre mSize == other.size()
+   assert(size() == other.size());
+   for (index_t i = 0; i < size(); ++i)
    {
-      VectorClock::indices_t indices;
-      utils::algo::copy_index_if(clock.begin(),
-                                 clock.end(),
-                                 std::inserter(indices, indices.end()),
-                                 pred,
-                                 0);
-      return indices;
+      (*this)[i] = std::max((*this)[i], other[i]);
    }
-   
-   //-------------------------------------------------------------------------------------
-    
-   VectorClock::values_t values_such_that(const VectorClock& clock,
-                                          const value_predicate_t& pred)
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void VectorClock::filter_values_greater_than(const VectorClock& other)
+{
+   /// @pre mSize == other.size()
+   assert(size() == other.size());
+   for (index_t i = 0; i < size(); ++i)
    {
-      VectorClock::values_t values;
-      std::copy_if(clock.begin(), clock.end(), std::inserter(values, values.end()), pred);
-      return values;
+      if ((*this)[i] <= other[i])
+      {
+         (*this)[i] = 0;
+      }
    }
-   
-   //-------------------------------------------------------------------------------------
+}
+
+//--------------------------------------------------------------------------------------------------
+
+VectorClock::value_t min_element(const VectorClock& clock)
+{
+   return *std::min_element(clock.begin(), clock.end());
+}
+
+//--------------------------------------------------------------------------------------------------
+
+VectorClock::value_t max_element(const VectorClock& clock)
+{
+   return *std::max_element(clock.begin(), clock.end());
+}
+
+//--------------------------------------------------------------------------------------------------
+
+VectorClock::indices_t indices_such_that(const VectorClock& clock, const value_predicate_t& pred)
+{
+   VectorClock::indices_t indices;
+   utils::algo::copy_index_if(clock.begin(), clock.end(), std::inserter(indices, indices.end()),
+                              pred, 0);
+   return indices;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+VectorClock::values_t values_such_that(const VectorClock& clock, const value_predicate_t& pred)
+{
+   VectorClock::values_t values;
+   std::copy_if(clock.begin(), clock.end(), std::inserter(values, values.end()), pred);
+   return values;
+}
+
+//--------------------------------------------------------------------------------------------------
 
 } // end namespace exploration
--- src/vector_clock.hpp
+++ src/vector_clock.hpp
@@ -1,75 +1,65 @@
 #pragma once
 
-// DATASTRUCTURES
 #include "fixed_size_vector.hpp"
 
-// STL
 #include <set>
 
-//--------------------------------------------------------------------------------------90
+//--------------------------------------------------------------------------------------------------
 /// @file vector_clock.hpp
-/// @brief Definition of class VectorClock.
 /// @author Susanne van den Elsen
 /// @date 2015-2016
-//----------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 
-namespace exploration
+
+namespace exploration {
+
+/// Let P be a program with threads from Tids and let E be an Execution of P. A
+/// VectorClock associated with the post State of a Transition t in E stores for each
+/// Thread::tid_t tid in Tids the index of the last Transition of that Thread in E
+/// happening-before t. The sequence of VectorClock objects associated with E
+/// represents a happens-before relation on E.
+
+class VectorClock : public datastructures::fixed_size_vector<int>
 {
-   //-------------------------------------------------------------------------------------
-   
-   /// Let P be a program with threads from Tids and let E be an Execution of P. A
-   /// VectorClock associated with the post State of a Transition t in E stores for each
-   /// Thread::tid_t tid in Tids the index of the last Transition of that Thread in E
-   /// happening-before t. The sequence of VectorClock objects associated with E
-   /// represents a happens-before relation on E.
-   
-   class VectorClock : public datastructures::fixed_size_vector<int>
-   {
-   public:
-      
-      using index_t = int;
-      using indices_t = std::set<index_t>;
-      using values_t = std::set<value_t>;
+public:
+   using index_t = int;
+   using indices_t = std::set<index_t>;
+   using values_t = std::set<value_t>;
 
-      /// @brief Constructs a n-size 0-value VectorClock.
+   /// @brief Constructs a n-size 0-value VectorClock.
 
-      explicit VectorClock(std::size_t n);
+   explicit VectorClock(std::size_t n);
 
-      /// @brief Transforms this VectorClock into max(*this, other) =
-      /// <max(*this[0], other[0]), ..., max(*this[mSize-1], other[mSize-1])>
+   /// @brief Transforms this VectorClock into max(*this, other) =
+   /// <max(*this[0], other[0]), ..., max(*this[mSize-1], other[mSize-1])>
 
-      void max(const VectorClock& other);
-      
-      /// @brief Transforms this VectorClock into gr(*this, other) =
-      /// <gr(*this[0], other[0], ..., gr(*this[mSize-1], other[mSize-1])>,
-      /// where gr(*this[i], other[i]) = *this[i] if *this[i] > other[i] and 0 otherwise.
+   void max(const VectorClock& other);
 
-      void filter_values_greater_than(const VectorClock& other);
-      
-   }; // end class VectorClock
-   
-   //-------------------------------------------------------------------------------------
-   
-   VectorClock::value_t min_element(const VectorClock& clock);
-   
-   VectorClock::value_t max_element(const VectorClock& clock);
-   
-   //-------------------------------------------------------------------------------------
-   
-   using value_predicate_t = std::function<bool(const VectorClock::value_t&)>;
-   
-   /// @brief Returns { i | pred(clock[i]) }.
-   
-   VectorClock::indices_t indices_such_that(const VectorClock& clock,
-                                            const value_predicate_t& pred);
-   
-   //-------------------------------------------------------------------------------------
-   
-   /// @brief Returns { clock[i] | pred(clock[i]) }.
-   
-   VectorClock::values_t values_such_that(const VectorClock& clock,
-                                          const value_predicate_t& pred);
-   
-   //-------------------------------------------------------------------------------------
-   
+   /// @brief Transforms this VectorClock into gr(*this, other) =
+   /// <gr(*this[0], other[0], ..., gr(*this[mSize-1], other[mSize-1])>,
+   /// where gr(*this[i], other[i]) = *this[i] if *this[i] > other[i] and 0 otherwise.
+
+   void filter_values_greater_than(const VectorClock& other);
+
+}; // end class VectorClock
+
+//--------------------------------------------------------------------------------------------------
+
+
+VectorClock::value_t min_element(const VectorClock& clock);
+
+VectorClock::value_t max_element(const VectorClock& clock);
+
+using value_predicate_t = std::function<bool(const VectorClock::value_t&)>;
+
+/// @brief Returns { i | pred(clock[i]) }.
+
+VectorClock::indices_t indices_such_that(const VectorClock& clock, const value_predicate_t& pred);
+
+/// @brief Returns { clock[i] | pred(clock[i]) }.
+
+VectorClock::values_t values_such_that(const VectorClock& clock, const value_predicate_t& pred);
+
+//--------------------------------------------------------------------------------------------------
+
 } // end namespace exploration
