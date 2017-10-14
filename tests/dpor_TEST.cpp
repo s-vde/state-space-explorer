dd6b974def2517dcd2bcf3309524e6adef84479c
Sanne <sanne-acc@posteo.net>
2017-10-14 19:10:31 +0200

    [submodules] update to new record-replay and cpp-utils commits

--- tests/dpor_TEST.cpp
+++ tests/dpor_TEST.cpp
@@ -20,21 +20,22 @@ namespace test {
 //--------------------------------------------------------------------------------------------------
 
 inline void dpor_number_of_executions_test(const boost::filesystem::path& test_program,
-   const std::string& compiler_options,
+                                           const std::string& optimization_level,
+                                           const std::string& compiler_options,
                                            const unsigned int expected_nr_executions)
 {
    const auto test_programs_dir = boost::filesystem::path(BOOST_PP_STRINGIZE(TEST_PROGRAMS_DIR));
    const auto tests_build_dir = boost::filesystem::path{BOOST_PP_STRINGIZE(TESTS_BUILD_DIR)};
    const auto output_dir = tests_build_dir / "test_data" / "test_programs_instrumented";
    scheduler::instrument((test_programs_dir / test_program).string(), output_dir.string(),
-                         compiler_options);
+                         optimization_level, compiler_options);
 
    const auto instrumented_program = (output_dir / test_program.stem()).replace_extension("");
 
    using dpor_t = Exploration<depth_first_search<dpor<Persistent>>>;
    dpor_t dpor{instrumented_program.string(), std::numeric_limits<unsigned int>::max()};
    dpor.run();
-   
+
    assert(dpor.statistics().nr_explorations() == expected_nr_executions);
 }
 
--- tests/main_TEST.cpp
+++ tests/main_TEST.cpp
@@ -11,11 +11,11 @@ int main()
    vector_clock_copy_constructor_from_matching_other_test();
 
    boost::filesystem::path test_programs_dir = BOOST_PP_STRINGIZE(TEST_PROGRAMS_DIR);
-   dpor_number_of_executions_test("shared_memory_access_non_concurrent.cpp", "-std=c++14", 1);
+   dpor_number_of_executions_test("shared_memory_access_non_concurrent.cpp", "0", "-std=c++14", 1);
    /// @cite Flanagan:2005:DPR:1040305.1040315 page 8
-   dpor_number_of_executions_test("../../libs/record-replay/tests/test_programs/real_world/filesystem.c", "-DNR_THREADS=13", 1);
+   dpor_number_of_executions_test("../../libs/record-replay/tests/test_programs/real_world/filesystem.c", "0", "-DNR_THREADS=13", 1);
    /// @cite Abdulla:2014:ODP:2535838.2535845
-   dpor_number_of_executions_test("benchmarks/readers_nonpreemptive.c", "", 5);
+   dpor_number_of_executions_test("benchmarks/readers_nonpreemptive.c", "0", "", 5);
    
    
    return 0;
